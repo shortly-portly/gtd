a1573d95ece856c8b7ef3fc48c465ddc0f71f4c9
David Simmons <shortlyportly@googlemail.com>
2014-10-17 20:00:29 +0100

    Save edited Notes + Text

---
+++ client/cards/editCard.js
@@ -0,0 +1,25 @@
+Template.editCard.events({
+  'click .cancel-edit': function(event) {
+
+    event.preventDefault();
+
+    Cards.update(this._id, {$set: {mode: 'view'}});
+
+    Session.set('cancelled', this._id);
+
+  },
+
+  'click .save-card': function(event, template) {
+    var title = template.find("input[name=title]");
+    var text = template.find("input[name=notes]");
+  
+    var data = {
+      title: title.value,
+      notes: text.value,
+      mode: 'view'
+    };
+
+    Cards.update(this._id, {$set: data});
+
+  }
+});
