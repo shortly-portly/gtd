8ed52c0a32b630d143d59de03744b4b0d38dec91
David Simmons <shortlyportly@googlemail.com>
2014-10-20 09:59:42 +0100

    Tidy up the journal entry function.

--- client/actions/actionJournal.js
+++ client/actions/actionJournal.js
@@ -2,18 +2,20 @@ Template.actionJournal.helpers({
   journalEntry: function() {
     var entries = Cards.find({parent: this._id, type: 'journal'}).fetch();
 
-    if (entries.lenght === 0 ) {
-      return entries;
-    }
-
     var currentDate = 0;
+
+    /* Only display the full date + time if this is the first time we've
+    /* displayed this journal entry. Subsequent journal entries for the same
+    /* date will only display the time. */
 
     _.each(entries, function(entry) {
 
-      if (moment(new Date(entry.createdAt)).isSame(new Date(currentDate), 'day')) {
-        entry.displayDate = moment(new Date(entry.createdAt)).format('hh:mm');
+      var createdAt = new Date(entry.createdAt);
+
+      if (moment(createdAt).isSame(new Date(currentDate), 'day')) {
+        entry.displayDate = moment(createdAt).format('hh:mm');
       } else {
-        entry.displayDate = moment(new Date(entry.createdAt)).format("dddd, MMMM Do YYYY, hh:mm a");
+        entry.displayDate = moment(createdAt).format("dddd, MMMM Do YYYY, hh:mm a");
       }
 
       currentDate = entry.createdAt;
